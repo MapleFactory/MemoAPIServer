9e5ff6d57dffe73e14913ca372f022271681cdb9
SungBeom <psb_94@naver.com>
2020-05-01 12:43:34 +0900

    sequelize를 이용한 User 테이블 생성 및 연동

---
+++ migrations/create-user.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('User', {
+      userId: {
+        type: Sequelize.STRING,
+        primaryKey: true
+      },
+      userPw: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      salt: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
