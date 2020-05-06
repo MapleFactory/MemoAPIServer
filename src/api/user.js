7d1a5aed0e88472421fcfcdbd17b3b738c12b398
SungBeom <psb_94@naver.com>
2020-05-06 22:52:52 +0900

    API에서 sequelize를 이용한 DB 연동

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,14 @@
 const Router = require('koa-router');
+const model = require('../../models');
 
 const userApi = new Router();
+
+model.sequelize.sync().then(() => {
+    console.log("DB connection success");
+}).catch(err => {
+    console.log("DB connection failed");
+    console.log(err);
+});
 
 userApi.get('/test', (ctx, next) => {
     console.log('/test');
